382533142e75f07bffa96ed5684b27b719011c41
Bernard Krehula <krehula.bernard456@gmail.com>
2026-08-13 21:01:43 +0200

    feat: add delete btn element

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import IconTrash from "~icons/mdi/trash";
+</script>
+
+<template>
+  <div class="main">
+    <button class="delete-btn">
+      <IconTrash />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  width: 300px;
+  margin: 100px auto;
+  background-color: gray;
+}
+.delete-btn{
+  cursor: pointer;
+  border: none;
+}
+</style>
